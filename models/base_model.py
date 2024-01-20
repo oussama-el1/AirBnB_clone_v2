b62bfc03ca7a2e398a8c5d9303f649674c8c7b0c
oussama-el1 <elhadraoui284@gmail.com>
2024-01-21 00:41:50 +0100

    task 10 boom

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,8 @@ if models.storage_t == "db":
 else:
     Base = object
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -29,14 +31,19 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
