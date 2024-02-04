b1c70aa26546cb77c5ef73cdb7b0ab34b9021f12
oussama-el1 <elhadraoui284@gmail.com>
2024-02-03 19:08:14 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,16 +14,17 @@ def do_deploy(archive_path):
         return False
     try:
         packe_name = os.path.basename(archive_path)
-        pwe = os.path.splitext(packe_name)
+        pwe = packe_name.split(".")[0]
         put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{pwe}/')
-        run(f'tar -xzf /tmp/{packe_name} -C /data/web_static/releases/{pwe}/')
-        run(f'rm /tmp/{packe_name}')
+        run('mkdir -p /data/web_static/releases/{}/'.format(pwe))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(packe_name, pwe))
+        run('rm /tmp/{}'.format(packe_name))
         path = '/data/web_static/releases/'
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, pwe))
         run('rm -rf {}{}/web_static'.format(path, pwe))
-        run(f'rm /data/web_static/current')
-        run(f'ln -s {path}{pwe}/ /data/web_static/current')
+        run('rm /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, pwe))
         return True
     except Exception as e:
         return False
