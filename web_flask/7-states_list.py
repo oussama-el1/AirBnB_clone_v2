ea8d8fe4d8def872d8017620b2eb565f2db6818f
oussama-el1 <elhadraoui284@gmail.com>
2024-02-19 09:59:45 +0100

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Lists of states"""
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
 from models import *
 from models import storage
-from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def fetchdata():
-    """display list of state"""
-    states = list(storage.all(State).values())
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', data=sorted_states)
+def states_list():
+    """HTML page with the states listed"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext()
-def removesession(exception):
-    """remove session"""
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
